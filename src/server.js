b7a87a8b3ec4407fcf2f6992c3174b54fc91ca23
monisahmad <monis.ahmad42@gmail.com>
2018-07-03 03:44:27 +0530

    Monis, Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 
+const PORT = process.env.SERVER_PORT;
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Server Started listening on port 3000!'));
+app.listen(PORT, () => console.log(`Server Started listening on port ${PORT}!`));
