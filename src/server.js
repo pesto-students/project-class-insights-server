7415a40551dbdc652d4d7b8ad80b7562fcd29925
Kundan Singh <kundan.singh979@gmail.com>
2018-07-03 08:55:28 +0530

    [FIX]: Single quotes for strings

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () =>
-  console.log(`Server Started listening on port ${PORT}!`)
-);
+app.listen(PORT, () => console.log(`Server Started listening on port ${PORT}!`));
