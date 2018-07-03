86083ed2b3fe74f2f7b4e42f9ac7e5fdf860f205
monisahmad <monis.ahmad42@gmail.com>
2018-07-03 21:43:32 +0530

    [Update] Server Side code

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const express = require('express');
 const PORT = process.env.SERVER_PORT;
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('HelloWorld!'));
 
 app.listen(PORT, () => console.log(`Server Started listening on port ${PORT}!`));
