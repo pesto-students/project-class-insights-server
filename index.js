66737d70c37e4867dc62f7b3f35ad947df86cf53
Anshu <4nshu19@gmail.com>
2018-07-17 04:45:08 +0530

    Revert "chore: Setting up travis to automatically deploy on heroku"
    This reverts commit f82004087724bd04e9d5f023a6ad581ff0f816ff.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-import 'babel-polyfill';
-import app from './server';
-import CONSTANTS from './lib/constants';
+import app from './src/server';
+import CONSTANTS from './src/lib/constants';
 
 const isProduction = process.env.NODE_ENV === CONSTANTS.NODE_ENV;
 const port = isProduction ? process.env.PORT : CONSTANTS.PORT;
